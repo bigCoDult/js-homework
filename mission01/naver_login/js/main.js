33343d6556258af435af1c27dc2b1a214d4a8144
bigCoDult <qqqqmmss11@gmail.com>
2023-07-17 01:17:38 +0900

    poorly done

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -1,73 +1,44 @@
-/*
+user = {
+  id: "asd@naver.com",
+  pw: "spdlqj123!@",
+};
 
-1. email 정규표현식을 사용한 validation
-2. pw 정규표현식을 사용한 validation
-3. 상태 변수 관리
-4. 로그인 버튼을 클릭시 조건처리
-
-*/
+const id = document.querySelector("#userEmail");
+const pw = document.querySelector("#userPassword");
+const submitButton = document.querySelector("#submitButton");
 
 function emailReg(text) {
   const re =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  //여기서 정규표현식이 하는 일 :
-
   return re.test(String(text).toLowerCase());
 }
 
 function pwReg(text) {
   const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
-  //여기서 정규표현식이 하는 일 :
-
   return re.test(String(text).toLowerCase());
 }
 
-// input.classList.add('is-active'); // input.classList.remove('is-active');
+function validateInput(inputElement, validatorFunc, className) {
+  inputElement.addEventListener("input", function () {
+    const inputText = inputElement.value;
 
-// ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-
-const input1 = document.querySelector("user-email-input");
-
-console.log(input1);
-
-login;
-
-class user1 extends class1 {
-  //클래스 확장으로 벨리데이션 할 id/pw부여
-  constructor(name) {
-    this.id = "asd@naver.com";
-    this.pw = "spdlqj123!@";
-  }
-
-  // set eat(food) {
-  //   this.stomach.push(food);
-  // }
-  // get eat() {
-  //   this.stomach;
-  // }
+    if (validatorFunc(inputText)) {
+      inputElement.classList.add(className);
+    } else {
+      inputElement.classList.remove(className);
+    }
+  });
 }
-let mission01 {
-  getId(){
-    const id1 = document.querySelectorAll(#userEmail);
-    return id1;
-  },
-  getPw(){
-    const pw1 = document.querySelectorAll(#userPassword);
-    return pwd1;
-  },
 
-  getinputText() {
-    inputId.addEventListener('input',()⇒{ 
-      
-    }
-    inputPw.addEventListener('input',()⇒{ 
-      
-    }
-  }
-    // function 입력있으면 트랙킹
-    // function 트랙킹된 텍스트가 유의미한 양식인지 판단
-    // function 판단에 따른 클래스 부여
-    // function 클래스를 판단
-    // function 클래스
-    
-  }
+validateInput(id, emailReg, "properId");
+validateInput(pw, pwReg, "properPw");
+
+function loginWell() {
+  if (
+    id.classList.contains("properId") &&
+    pw.classList.contains("properPw") &&
+    id.value == "asd@naver.com" &&
+    pw.value == "spdlqj123!@"
+  )
+  window.location.href = "./welcome.html";
+}
